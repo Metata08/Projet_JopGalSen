edc92ba1ab7198dd94b463db23cd82b850369f04
Metata08 <126245694+Metata08@users.noreply.github.com>
2025-10-22 00:57:34 +0000

    ok pour le demarrage

--- JopGalSen-final/src/main/java/com/mycompany/myapp/repository/NotificationRepository.java
+++ JopGalSen-final/src/main/java/com/mycompany/myapp/repository/NotificationRepository.java
@@ -1,11 +1,14 @@
 package com.mycompany.myapp.repository;
 
-import com.mycompany.myapp.domain.Notification;
 import java.util.List;
-import org.springframework.data.jpa.repository.*;
-import org.springframework.stereotype.Repository;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import com.mycompany.myapp.domain.Notification;
 
 /**
  * Spring Data JPA repository for the Notification entity.
@@ -14,5 +17,8 @@ import org.springframework.data.domain.Pageable;
 @Repository
 public interface NotificationRepository extends JpaRepository<Notification, Long> {
     @Query("select notification from Notification notification where notification.user.login = ?#{authentication.name}")
+    List<Notification> findByUserIsCurrentUser();
+
+ @Query("select notification from Notification notification where notification.user.login = :#{principal.username}")
     Page<Notification> findByUserIsCurrentUser(Pageable pageable);
 }
